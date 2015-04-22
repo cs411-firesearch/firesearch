34671c5a2bf63180907230868aa6adbebdeeeed4
Haozhen Ding <hzding621@gmail.com>
2015-04-22 00:15:46 -0500

    remove

--- sql/Own.sql
+++
@@ -1,9 +0,0 @@
-/*CREATE TABLE*/
-CREATE TABLE Own(
-	Volume INT DEFAULT 1,
- 	Price FLOAT DEFAULT 0,
- 	StockId INT not null,
- 	UserId INT not null,
-	FOREIGN KEY (StockId) REFERENCES Stock(StockId),
-	FOREIGN KEY (UserId) REFERENCES User(UserId)
-);
--- sql/Recommend.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Recommend (
- 	UserId INT not null,
- 	StockId INT not null,
- 	Rate INT not null,
-	FOREIGN KEY (StockId) REFERENCES Stock(StockId),
-	FOREIGN KEY (UserId) REFERENCES User(UserId)
-)
